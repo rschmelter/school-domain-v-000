a1b6b6b56bc776a3ecce716e75d9a72a8535f4db
Ryan Schmelter <rschmelter33@gmail.com>
2017-11-08 18:06:16 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+
+class School
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    roster.collect do |grade, name|
+      roster[grade].sort do |a , b|
+        a <=> b
+      end
+    end
+
+  end
+
+end
